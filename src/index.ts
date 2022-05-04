99a4e1cfa6cd1741d58b29fd274373337197a09c
freddyshim <shimfreddy@gmail.com>
2022-05-04 15:56:02 -0400

    changed port

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
-import passport from 'passport';
-import bodyParser from 'body-parser';
-import { Keys } from './config/Keys';
-import { AppRouter } from './AppRouter';
-import { Network } from './utils/Network';
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import bodyParser from 'body-parser'
+import { Keys } from './config/Keys'
+import { AppRouter } from './AppRouter'
+import { Network } from './utils/Network'
 
-import './models/User';
-import './services/passport';
-import './controllers/RootController';
-import './controllers/AuthController';
+import './models/User'
+import './services/passport'
+import './controllers/RootController'
+import './controllers/AuthController'
 
 // initialize mongoose
 mongoose.connect(
@@ -23,25 +23,25 @@ mongoose.connect(
   (error): void => {
     if (error) {
       if (process.env.NODE_ENV !== 'production') {
-        console.error(error);
+        console.error(error)
       }
-      console.log('Connection to database failed. Try restarting the server.');
+      console.log('Connection to database failed. Try restarting the server.')
     }
   }
-);
+)
 
 // initialize express
-const app = express();
+const app = express()
 
 // initialize express middleware
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
 
 // initialize routes
-app.use(AppRouter.instance);
+app.use(AppRouter.instance)
 
 // start server
-app.listen(3000, () => {
-  console.log(`Server is running at ${Network.address}`);
-});
+app.listen(8080, () => {
+  console.log(`Server is running at ${Network.address}`)
+})
