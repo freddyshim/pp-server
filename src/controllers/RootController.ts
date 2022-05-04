b6368b19493d122bb872a34b1d3853dc89387071
freddyshim <shimfreddy@gmail.com>
2022-05-04 16:31:08 -0400

    added explicit 200 return code at base url

--- src/controllers/RootController.ts
+++ src/controllers/RootController.ts
@@ -1,31 +1,31 @@
-import { Request, Response } from 'express';
-import { Controller, Use, Get, Post } from './decorators';
-import { requireLogin } from '../middlewares/requireLogin';
-import { areUsersEqual } from '../utils/user';
-import { User, UserDocument } from '../models/User';
+import { Request, Response } from 'express'
+import { Controller, Use, Get, Post } from './decorators'
+import { requireLogin } from '../middlewares/requireLogin'
+import { areUsersEqual } from '../utils/user'
+import { User, UserDocument } from '../models/User'
 
 @Controller('')
 class RootController {
   @Get('/')
   getRoot(req: Request, res: Response): void {
-    res.send('Great success!');
+    res.status(200).send('Great success!')
   }
 
   @Get('/user')
   @Use(requireLogin)
   getUser(req: Request, res: Response): void {
-    res.send(req.user as UserDocument);
+    res.send(req.user as UserDocument)
   }
 
   @Post('/user')
   @Use(requireLogin)
   async postUser(req: Request, res: Response): Promise<void> {
-    const localUser = req.body as UserDocument;
-    const databaseUser = req.user as UserDocument;
+    const localUser = req.body as UserDocument
+    const databaseUser = req.user as UserDocument
     if (!areUsersEqual(localUser, databaseUser)) {
-      await User.updateOne({ id: localUser.id }, localUser);
+      await User.updateOne({ id: localUser.id }, localUser)
     }
 
-    res.send({});
+    res.send({})
   }
 }
