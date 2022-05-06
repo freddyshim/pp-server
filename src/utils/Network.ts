a501e4b70e28118f3c945c0cd61340305b7bc0ad
freddyshim <shimfreddy@gmail.com>
2022-05-05 22:16:47 -0400

    fixed twitch oauth redirect bug

--- src/utils/Network.ts
+++ src/utils/Network.ts
@@ -1,9 +1,9 @@
 export class Network {
   static get address(): string {
     if (process.env.NODE_ENV === 'production') {
-      return 'https://anookday.dev';
+      return 'https://pp.freddyshim.com'
     } else {
-      return 'http://localhost:3000';
+      return 'http://localhost:3000'
     }
   }
 }
