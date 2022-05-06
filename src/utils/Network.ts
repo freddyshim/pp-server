2f06ad9fde216eaff2d65d0fce2bf9d172a578f7
freddyshim <shimfreddy@gmail.com>
2022-05-05 22:54:19 -0400

    changed domain name

--- src/utils/Network.ts
+++ src/utils/Network.ts
@@ -1,9 +1,3 @@
 export class Network {
-  static get address(): string {
-    if (process.env.NODE_ENV === 'production') {
-      return 'https://pp.freddyshim.com'
-    } else {
-      return 'http://localhost:3000'
-    }
-  }
+  static address: string = 'https://pp.freddyshim.com'
 }
